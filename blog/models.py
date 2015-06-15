1d49494a87f7eb6958a9eb8097c4ea4197b57d32
Anton Makrushyn <ansermak@gmail.com>
2015-06-15 17:51:46 +0300

    Change models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
@@ -11,7 +12,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(Author)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    change_date = models.DateTimeField('date_changed', auto_now=True)
 
     def __str__(self):
         return "{} by {}".format(self.title, self.author)
@@ -19,8 +21,8 @@ class Post(models.Model):
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
-    file_name = models.CharField(max_length=256)
     post = models.ForeignKey(Post)
+    image_file = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
